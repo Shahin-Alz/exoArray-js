87a6999df25d0edfb7a21d55240b698002694cb1
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-20 10:56:16 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+// exo1
+
+// let coding = ["coding20", "coding21", "coding22"]
+
+// console.log(coding);
+
+// console.log(coding.length);
+
+// exo2
+
+// let coding = ["coding23", "coding24", "coding25"]
+
+// console.log(coding.push("shahin"));
+
+// console.log(coding);
+
+// exo3
+
+// let coding = ["coding23", "coding24", "coding25"]
+
+// console.log(coding);
+
+// console.log(coding.push("shahin"));
+
+// console.log(coding);
+
+// coding.splice(3, 1, 'alemzadeh')
+
+// console.log(coding);
+
+// exo4
+
+let coding = ["coding23", "coding24", "coding25"]
+
+console.log(coding);
+
+console.log(coding.push("shahin"));
+
+console.log(coding);
+
+coding.splice(3, 1, 'alemzadeh')
+
+console.log(coding);
+
+coding.shift();
+
+console.log(coding);
+
+
+
+
+
+
+
+
